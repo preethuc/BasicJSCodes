d4ba256ab643f16c40ec805106a96b3946187e7e
preethuc <preetha@sparkouttech.com>
2022-09-13 12:50:44 +0530

    multer code

---
+++ multer/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const multer = require('multer');
+
+const app = express();
+ 
+const fileStorageEngine = multer.diskStorage({
+    destination: (req,file,cb) => {
+        cb(null,'./images');    
+    },
+    filename: (req,file,cb) => {
+        cb(null , Date.now() + file.originalname);   
+    }
+});
+
+//middleware
+const upload = multer({storage: fileStorageEngine});
+
+//Route
+//single image
+app.post('/api/single',upload.single('image'),(req,res)=>{
+    console.log(req.file);
+    res.send('single file upload success')
+})
+//multiple images
+app.post('/api/multiple',upload.array('images', 3),(req,res)=>{
+    console.log(req.files);
+    res.send('multiple file upload success')
+})
+
+
+//server
+app.listen(3000);
