efa9ff386b63b0f7f491415f82e1893933edb2f2
preethuc <preetha@sparkouttech.com>
2022-09-15 12:58:16 +0530

    socket code newly added

---
+++ Sockets/index.js
@@ -0,0 +1,36 @@
+// const express = require('express');
+// const app = express();
+var app = require('express')();
+// const http = require('http');
+// const server = http.createServer(app)
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+// const io = require('socket.io')( server,{ cors:{ origin: '*' } } 
+
+app.get('/', function(req, res){
+  //send the index.html file for all requests
+  res.sendFile(__dirname + '/index.html');
+
+});
+
+//client connection to the server
+io.on('connection', (socket,userID) => {
+   userID=Math.floor(Math.random() * 10000)
+   //userID=socket.id
+   //console.log(socket);
+   console.log('an user connected as ' +userID);
+   socket.on('disconnect',()=>{
+   console.log('user disconnected ' +userID)
+  })
+  socket.on('chat message', (msg) => {
+    socket.broadcast.emit('chat message', msg,userID);
+    console.log(userID +":"+ msg)
+  });
+
+});
+
+http.listen(3001, function(){
+
+  console.log('listening on 3001.....');
+
+});
---
+++ Sockets/rough.js
@@ -0,0 +1,11 @@
+// const target = { a: 1, b: 2 };
+// const source = { b: 4, c: 5 };
+// console.log(target);
+// const returnedTarget = Object.assign(source,target);
+
+// // expected output: Object { a: 1, b: 4, c: 5 }
+
+// console.log(source);
+
+
+// console.log(returnedTarget);
